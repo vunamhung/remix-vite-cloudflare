3e0c32ee365bed636b88a23a43fd1e61217d0ef6
Hung Vu <vunamhung@gmail.com>
2024-05-16 19:36:25 +0700

    :recycle: Refactoring code.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,6 +7,29 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
   root: true,
+  rules: {
+    'valtio/state-snapshot-rule': 1,
+    'valtio/avoid-this-in-proxy': 1,
+    'no-console': [1, { allow: ['warn', 'error'] }],
+    'no-unused-vars': 1,
+    'no-new-object': 1,
+    'no-array-constructor': 1,
+    'no-new-func': 1,
+    'no-var': 1,
+    'object-shorthand': 1,
+    'no-prototype-builtins': 1,
+    'no-eval': 1,
+    'prefer-spread': 1,
+    'no-param-reassign': 1,
+    'prefer-arrow-callback': 1,
+    'no-confusing-arrow': 1,
+    'no-useless-constructor': 1,
+    'no-dupe-class-members': 1,
+    'prefer-const': 1,
+    'no-multi-assign': 1,
+    'no-plusplus': 1,
+    eqeqeq: 1,
+  },
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
@@ -19,22 +42,17 @@ module.exports = {
     commonjs: true,
     es6: true,
   },
+  ignorePatterns: ['!**/.server', '!**/.client'],
 
   // Base config
-  extends: ['eslint:recommended'],
+  extends: ['eslint:recommended', 'plugin:valtio/recommended', 'prettier'],
 
   overrides: [
     // React
     {
       files: ['**/*.{js,jsx,ts,tsx}'],
       plugins: ['react', 'jsx-a11y'],
-      extends: [
-        'plugin:react/recommended',
-        'plugin:react/jsx-runtime',
-        'plugin:react-hooks/recommended',
-        'plugin:jsx-a11y/recommended',
-        'plugin:valtio/recommended',
-      ],
+      extends: ['plugin:react/recommended', 'plugin:react/jsx-runtime', 'plugin:react-hooks/recommended', 'plugin:jsx-a11y/recommended'],
       settings: {
         react: {
           version: 'detect',
@@ -71,7 +89,7 @@ module.exports = {
 
     // Node
     {
-      files: ['.eslintrc.js'],
+      files: ['.eslintrc.cjs'],
       env: {
         node: true,
       },
